43d7be00157dd63776424e2c11db13751c349e42
MIIMIU <86527298+Mimiu2021@users.noreply.github.com>
2021-06-27 11:00:13 +0800

    Delete HelloWorld01.java

--- HelloWorld01.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld01 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-    }
-}
